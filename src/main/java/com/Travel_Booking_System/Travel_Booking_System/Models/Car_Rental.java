786cf8e556678c78b45de1f6186dc68efbb08aed
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-10 14:25:06 +0400

    add code into car rental model class

--- src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Car_Rental.java
+++ src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Car_Rental.java
@@ -1,4 +1,26 @@
 package com.Travel_Booking_System.Travel_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "car_rental")
 public class Car_Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long car_rental_id;
+    Integer car_model;
+    String rental_agency;
+    String pickup_location;
+    String return_location;
+    String rental_period;
+    Double price;
+    boolean availability;
+    
 }
