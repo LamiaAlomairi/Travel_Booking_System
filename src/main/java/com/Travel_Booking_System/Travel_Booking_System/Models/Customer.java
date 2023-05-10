0d4325a98612d9bddf1c84eb36efb101baaa34e0
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-10 12:35:04 +0400

    add attributes into customer model class

--- src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Customer.java
+++ src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Customer.java
@@ -1,4 +1,23 @@
 package com.Travel_Booking_System.Travel_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long customer_id;
+    String name;
+    Integer phone_number;
+    String address;
+    String payment_information;
+
 }
