b571383be47dc40303baa018696fa70790440502
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-10 12:42:52 +0400

    add attributes into flight model class

--- src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Flight.java
+++ src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Flight.java
@@ -1,4 +1,27 @@
 package com.Travel_Booking_System.Travel_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "flight")
 public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long flight_id;
+    String airline;
+    String departure_city;
+    String arrival_city;
+    String departure_time;
+    String arrival_time;
+    Integer duration;
+    Double price;
+    boolean availability;
+
 }
