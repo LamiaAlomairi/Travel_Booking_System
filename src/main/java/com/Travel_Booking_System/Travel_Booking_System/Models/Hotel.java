d738afd6cd538bf80ea840efe82ce62871469911
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-10 12:38:42 +0400

    add attributes into hotel model class

--- src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Hotel.java
+++ src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Hotel.java
@@ -1,4 +1,24 @@
 package com.Travel_Booking_System.Travel_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "hotel")
 public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long hotel_id;
+    String name;
+    String address;
+    Integer rating;
+    String room_types;
+    String amenities;
+    boolean availability;
 }
