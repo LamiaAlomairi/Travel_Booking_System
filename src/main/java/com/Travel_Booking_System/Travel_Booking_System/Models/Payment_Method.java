de013086921d68714242b0899030fb7b36b24dbb
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-10 14:37:15 +0400

    add attributes into rating, payment method classes

--- src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Payment_Method.java
+++ src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Payment_Method.java
@@ -1,4 +1,23 @@
 package com.Travel_Booking_System.Travel_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "payment_method")
 public class Payment_Method {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long payment_method_id;
+    String name;
+    Integer card_number;
+    String expiration_date;
+    String security_code;
+
 }
--- src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Rating.java
+++ src/main/java/com/Travel_Booking_System/Travel_Booking_System/Models/Rating.java
@@ -1,4 +1,19 @@
 package com.Travel_Booking_System.Travel_Booking_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "rating")
 public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long rating_id;
+    String rating;
 }
